954f020d6caf272d8bea7d78e688197ac298e0cc
tduyng <tduyng.dev@gmail.com>
2024-09-16 09:02:17 +0200

    feat: parse string

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -9,6 +9,7 @@ impl Interpreter {
 
     pub fn evaluate(&mut self, expr: Expr) -> String {
         match expr {
+            Expr::String(s) => s,
             Expr::Number(n) => n.to_string(),
             Expr::Boolean(b) => b.to_string(),
             Expr::Nil => "nil".to_string(),
--- src/parser.rs
+++ src/parser.rs
@@ -22,10 +22,16 @@ impl Parser {
         if !self.is_at_end() {
             let token = self.advance();
             match token.token_type {
+                TokenType::String => Expr::String(token.lexeme.clone()),
                 TokenType::Number => Expr::Number(token.lexeme.parse().unwrap_or(0.0)),
                 TokenType::True => Expr::Boolean(true),
                 TokenType::False => Expr::Boolean(false),
                 TokenType::Nil => Expr::Nil,
+                TokenType::LeftParen => {
+                    let expr = self.parse();
+                    self.consume(TokenType::RightParen);
+                    expr
+                }
                 _ => {
                     self.report_error(ScannerError::UnexpectedCharacter(' ', 1));
                     Expr::Nil
@@ -55,9 +61,22 @@ impl Parser {
         eprintln!("{}", error);
         self.errors.push(error);
     }
+
+    fn peek(&self) -> &Token {
+        &self.tokens[self.current]
+    }
+
+    fn consume(&mut self, expected: TokenType) {
+        if self.is_at_end() || self.peek().token_type != expected {
+            self.report_error(ScannerError::UnexpectedCharacter(' ', self.current));
+        } else {
+            self.advance();
+        }
+    }
 }
 
 pub enum Expr {
+    String(String),
     Number(f64),
     Boolean(bool),
     Nil,
