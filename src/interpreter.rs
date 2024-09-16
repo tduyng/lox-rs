027f9ac4d20d42b33ac680a849db5ffdab15f4fd
tduyng <tduyng.dev@gmail.com>
2024-09-16 11:22:58 +0200

    feat: handle runtime parser error

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -15,18 +15,23 @@ impl Interpreter {
             Expr::Nil => Expr::Nil,
             Expr::Unary { operator, right } => {
                 let right_val = self.evaluate(*right);
+                let line_number = operator.line;
                 match operator.token_type {
                     TokenType::Minus => {
                         if let Expr::Number(n) = right_val {
                             return Expr::Number(-n);
                         }
-                        panic!("Operand must be a number.");
+                        eprintln!("Operand must be a number.\n[line {}]", line_number);
+                        std::process::exit(70);
                     }
                     TokenType::Bang => {
                         let is_truthy = self.is_truthy(&right_val);
-                        return Expr::Boolean(!is_truthy);
+                        Expr::Boolean(!is_truthy)
                     }
-                    _ => panic!("Unknown unary operator."),
+                    _ => {
+                        eprintln!("Unknown unary operator.\n[line {}]", line_number);
+                        std::process::exit(70);
+                    }
                 }
             }
             Expr::Binary {
@@ -36,91 +41,106 @@ impl Interpreter {
             } => {
                 let left_val = self.evaluate(*left);
                 let right_val = self.evaluate(*right);
-                self.handle_binary_op(left_val, &operator.token_type, right_val)
+                let line = operator.line;
+                self.handle_binary_op(left_val, &operator.token_type, right_val, line)
             }
         }
     }
 
-    fn handle_binary_op(&self, left: Expr, operator: &TokenType, right: Expr) -> Expr {
+    fn handle_binary_op(&self, left: Expr, operator: &TokenType, right: Expr, line: usize) -> Expr {
         match operator {
-            TokenType::Plus => self.handle_plus(left, right),
-            TokenType::Minus => self.handle_minus(left, right),
-            TokenType::Slash => self.handle_divide(left, right),
-            TokenType::Star => self.handle_multiply(left, right),
-            TokenType::Greater => self.handle_greater(left, right),
-            TokenType::GreaterEqual => self.handle_greater_equal(left, right),
-            TokenType::Less => self.handle_less(left, right),
-            TokenType::LessEqual => self.handle_less_equal(left, right),
+            TokenType::Plus => self.handle_plus(left, right, line),
+            TokenType::Minus => self.handle_minus(left, right, line),
+            TokenType::Slash => self.handle_divide(left, right, line),
+            TokenType::Star => self.handle_multiply(left, right, line),
+            TokenType::Greater => self.handle_greater(left, right, line),
+            TokenType::GreaterEqual => self.handle_greater_equal(left, right, line),
+            TokenType::Less => self.handle_less(left, right, line),
+            TokenType::LessEqual => self.handle_less_equal(left, right, line),
             TokenType::EqualEqual => self.handle_equal_equal(left, right),
             TokenType::BangEqual => self.handle_bang_equal(left, right),
             _ => Expr::Nil,
         }
     }
 
-    fn handle_plus(&self, left: Expr, right: Expr) -> Expr {
+    fn handle_plus(&self, left: Expr, right: Expr, line: usize) -> Expr {
         match (left, right) {
             (Expr::Number(l), Expr::Number(r)) => Expr::Number(round(l + r, 2)),
             (Expr::String(l), Expr::String(r)) => Expr::String(format!("{}{}", l, r)),
-            _ => panic!("Operands must be two numbers or two strings."),
+            _ => {
+                eprintln!(
+                    "Operands must be two numbers or two strings.\n[line {}]",
+                    line
+                );
+                std::process::exit(70);
+            }
         }
     }
 
-    fn handle_minus(&self, left: Expr, right: Expr) -> Expr {
+    fn handle_minus(&self, left: Expr, right: Expr, line: usize) -> Expr {
         if let (Expr::Number(l), Expr::Number(r)) = (left, right) {
             Expr::Number(round(l - r, 2))
         } else {
-            panic!("Operands must be numbers.");
+            eprintln!("Operands must be numbers.\n[line {}]", line);
+            std::process::exit(70);
         }
     }
 
-    fn handle_divide(&self, left: Expr, right: Expr) -> Expr {
+    fn handle_divide(&self, left: Expr, right: Expr, line: usize) -> Expr {
         if let (Expr::Number(l), Expr::Number(r)) = (left, right) {
             if r == 0.0 {
-                panic!("Division by zero.");
+                eprintln!("Division by zero.\n[line {}]", line);
+                std::process::exit(70);
             }
             Expr::Number(round(l / r, 2))
         } else {
-            panic!("Operands must be numbers.");
+            eprintln!("Operands must be numbers.\n[line {}]", line);
+            std::process::exit(70);
         }
     }
 
-    fn handle_multiply(&self, left: Expr, right: Expr) -> Expr {
+    fn handle_multiply(&self, left: Expr, right: Expr, line: usize) -> Expr {
         if let (Expr::Number(l), Expr::Number(r)) = (left, right) {
             Expr::Number(round(l * r, 2))
         } else {
-            panic!("Operands must be numbers.");
+            eprintln!("Operands must be numbers.\n[line {}]", line);
+            std::process::exit(70);
         }
     }
 
-    fn handle_greater(&self, left: Expr, right: Expr) -> Expr {
+    fn handle_greater(&self, left: Expr, right: Expr, line: usize) -> Expr {
         if let (Expr::Number(l), Expr::Number(r)) = (left, right) {
             Expr::Boolean(l > r)
         } else {
-            panic!("Operands must be numbers.");
+            eprintln!("Operands must be numbers.\n[line {}]", line);
+            std::process::exit(70);
         }
     }
 
-    fn handle_greater_equal(&self, left: Expr, right: Expr) -> Expr {
+    fn handle_greater_equal(&self, left: Expr, right: Expr, line: usize) -> Expr {
         if let (Expr::Number(l), Expr::Number(r)) = (left, right) {
             Expr::Boolean(l >= r)
         } else {
-            panic!("Operands must be numbers.");
+            eprintln!("Operands must be numbers.\n[line {}]", line);
+            std::process::exit(70);
         }
     }
 
-    fn handle_less(&self, left: Expr, right: Expr) -> Expr {
+    fn handle_less(&self, left: Expr, right: Expr, line: usize) -> Expr {
         if let (Expr::Number(l), Expr::Number(r)) = (left, right) {
             Expr::Boolean(l < r)
         } else {
-            panic!("Operands must be numbers.");
+            eprintln!("Operands must be numbers.\n[line {}]", line);
+            std::process::exit(70);
         }
     }
 
-    fn handle_less_equal(&self, left: Expr, right: Expr) -> Expr {
+    fn handle_less_equal(&self, left: Expr, right: Expr, line: usize) -> Expr {
         if let (Expr::Number(l), Expr::Number(r)) = (left, right) {
             Expr::Boolean(l <= r)
         } else {
-            panic!("Operands must be numbers.");
+            eprintln!("Operands must be numbers.\n[line {}]", line);
+            std::process::exit(70);
         }
     }
 
